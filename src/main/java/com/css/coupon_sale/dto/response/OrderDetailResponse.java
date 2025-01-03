f1ce69bf01bd4ee815dc10df7d0e59fa6f8d526f
ZawMyoThantSin <zawmyothantzin37@gmail.com>
2025-01-03 10:15:07 +0630

    order accept

---
+++ src/main/java/com/css/coupon_sale/dto/response/OrderDetailResponse.java
@@ -0,0 +1,143 @@
+package com.css.coupon_sale.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderDetailResponse {
+    private int id;
+    private int order_id;
+    private long user_id;
+    private int quantity;
+    private int totalPrice;
+    private int status;
+    private String screenshot;
+    private LocalDateTime order_date;
+    private String message;
+
+    private String userName;
+    private String phoneNumber;
+    private String userEmail;
+
+    private PaymentInfo paymentInfo;
+    private List<OrderItems> orderItems;
+
+
+
+    public record PaymentInfo(String accountName, String accountNumber, String paymentType){}
+    public record OrderItems(String name,String imagePath, double unitPrice, int quantity){}
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getScreenshot() {
+        return screenshot;
+    }
+
+    public void setScreenshot(String screenshot) {
+        this.screenshot = screenshot;
+    }
+
+    public LocalDateTime getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(LocalDateTime order_date) {
+        this.order_date = order_date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public PaymentInfo getPaymentInfo() {
+        return paymentInfo;
+    }
+
+    public void setPaymentInfo(PaymentInfo paymentInfo) {
+        this.paymentInfo = paymentInfo;
+    }
+
+    public List<OrderItems> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItems> orderItems) {
+        this.orderItems = orderItems;
+    }
+}
+
+
